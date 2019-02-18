7888eacb1c25ebe8952694a5c4ffe55f32c446ed
Qingbo <tianliang657@163.com>
2019-02-18 11:39:34 +0800

    Initial commit

---
+++ lib/riil_helper.py
@@ -0,0 +1,32 @@
+# coding=utf-8
+import configparser
+import xlrd
+
+
+def get_data_from_excel(url):
+    ff = xlrd.open_workbook(u'../../../../data/cheetah_get_element_xpath.xlsx')
+    table = ff.sheet_by_index(0)
+    xpath = ''
+    for i in range(1, table.nrows):
+        # print(table.cell(i, 0).value)
+        if table.cell(i, 0).value == url:
+            xpath = str(table.cell(i, 1).value)
+            break
+    print(xpath)
+    return xpath
+
+
+def get_xpath(section, name):
+    cf = configparser.ConfigParser()
+    cf.read(u'../../../../data/cheetah_element_xpath.conf')
+    # print(cf.get(section, name))
+    return cf.get(section, name)
+
+
+def get_common_search(search_type, search_element_name):
+    print("//" + search_type + "[contains(text(), '" + search_element_name + "')]")
+    return str("//" + search_type + "[contains(text(), '" + search_element_name + "')]")
+
+
+# if __name__ == '__main__':
+#     print(get_data_from_excel())
