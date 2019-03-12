915674ad1bcfa8b9396affed752a15316aad76d1
Qingbo <tianliang657@163.com>
2019-03-12 08:31:41 +0800

    update bat

---
+++ Jenkins/cheetah/菜单栏/系统配置/WIN10_Cheetah_系统配置.bat
@@ -0,0 +1,3 @@
+cd C:\Jenkins\workspace\Win10_robot\RIIL_automation_testing_base_on_robot_framework-dev\testcases\cheetah\菜单栏\系统配置
+robot -d C:\Jenkins\workspace\Win10_robot\RIIL_automation_testing_base_on_robot_framework-dev\testcases\cheetah\test_results\IPGM_results IP分组管理.robot
+robot -d C:\Jenkins\workspace\Win10_robot\RIIL_automation_testing_base_on_robot_framework-dev\testcases\cheetah\test_results\PM_results 位置管理.robot
